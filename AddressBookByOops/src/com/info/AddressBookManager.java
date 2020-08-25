a10c7a3c98016627b7074deec3caef1a64081559
pradip0016 <patilpradip980@gmail.com>
2020-08-25 16:33:41 +0530

    Added NewAddressBook function

--- AddressBookByOops/src/com/info/AddressBookManager.java
+++ AddressBookByOops/src/com/info/AddressBookManager.java
@@ -1,19 +1,55 @@
 package com.info;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class AddressBookManager implements AddressBookManagerInterface {
+	public String fileName;
+	public String fileuse;
+	public String option;
+	public FileReader fr;
+	public FileWriter fileWriter;
+	public BufferedWriter bw;
+	public static File file;
+	public String regexphone="[0-9]{10}";
+	public static String path = "E:\\Pradip\\AddressBookUsingOops\\csv\\";
+	public static Scanner sc = new Scanner(System.in);
+	public ArrayList<Person> personarraylist12 = new ArrayList<Person>(100);
+	public HashMap<String,ArrayList<Person>> personarraylist1o = new HashMap<>(100);
 
 	@Override
 	public File newaddressbook() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.print("Enter the desired name of your Book: ");
+		fileName = sc.next();
+		fileName = fileName + ".csv";
+		file = new File(path + fileName);
+		System.out.println("Do U want To create Address Book Y/N");
+		String op = sc.next();
+		if (op.equalsIgnoreCase("Y")) {
+			file.createNewFile();
+			fileWriter = new FileWriter(file, true);
+			bw = new BufferedWriter(fileWriter);
+			bw.write("FirstName");
+			bw.write(",Lastname");
+			bw.write(",City");
+			bw.write(",State");
+			bw.write(",Zipcode");
+			bw.write(",Phonenumber");
+			bw.newLine();
+			System.out.println("Address Book Created ");
+		} else {
+			System.out.println("Address Book Not created already present");
+		}
+		bw.close();
+		fileWriter.close();
+		return file;
 	}
-
 	@Override
 	public HashMap<String, ArrayList<Person>> openaddressbook()
 			throws ReflectiveOperationException, RuntimeException, IOException {
--- AddressBookByOops/src/com/info/AddressBookoops.java
+++ AddressBookByOops/src/com/info/AddressBookoops.java
@@ -1,10 +1,47 @@
 package com.info;
 
+import java.io.*;
+import java.util.*;
+
 public class AddressBookoops {
+	public static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException, ReflectiveOperationException, RuntimeException {
+		while (true) {
+			System.out.println("1) create new Address Book \n" + "2) Open Address Book \n" + "3) Save Address Book \n"
+					+ "4) Save As Address Book \n" + "5) Close Address Book \n" + "6) Quit \n"
+					+ "7) Show AddressBooks\n");
+			System.out.println("Please Enter Your Choice \n");
+			int ch = sc.nextInt();
+			AddressBookManager AddressbookManagerImplementation = new AddressBookManager();
+			switch (ch) {
+			case 1:
+				AddressbookManagerImplementation.newaddressbook();
+				break;
+			case 2:
+				AddressbookManagerImplementation.openaddressbook();
 
+				break;
+			case 3:
+				// AddressbookManagerImplementation.saveaddressbook();
+				break;
+			case 4:
+				AddressbookManagerImplementation.saveasaddressbook();
+				break;
+			case 5:
+				// AddressbookManagerImplementation.closeaddressbook();
+				break;
+			case 6:
+				AddressbookManagerImplementation.quit();
+				break;
+			case 7:
+				AddressbookManagerImplementation.showAddressBook();
+				break;
+			default:
+				System.out.println("Wrong choice please enter choice from above menu");
+
+			}
+
+		}
 	}
-
 }
